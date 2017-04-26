c8ca73d41abc84b4edc9346f49234149da94eb67
xxros <liusongh@gmail.com>
2017-04-27 03:26:15 +0800

    simplize helloworld

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,13 +3,8 @@
 
 using namespace std;
 
-//main();
-//
-//
-
 int main()
 {
-    //cout << "Hello world"; //  output hello world
     cout << "Hello world" << endl; //  output hello world
     return 0;
 }
